b29b60ba3c31e51516812143fd4ceb5b4db0626a
Alexandr Stelnykovych <alexandr.stelnykovych@ivpn.net>
2020-02-13 11:40:14 +0200

    fix: Firewall changes from Always-On to On-Demand after upgrade

--- service/preferences.go
+++ service/preferences.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"strings"
 
 	"github.com/ivpn/desktop-app-daemon/service/platform"
 )
@@ -29,8 +30,39 @@ func (s *Preferences) savePreferences() error {
 }
 func (s *Preferences) loadPreferences() error {
 	data, err := ioutil.ReadFile(platform.SettingsFile())
+
 	if err != nil {
 		return fmt.Errorf("failed to read preferences file: %w", err)
+	}
+
+	dataStr := string(data)
+	if strings.Contains(dataStr, `"firewall_is_persistent"`) {
+		// It is a first time loading preferences after IVPN Client upgrade from old version (<= v2.10.9)
+		// Loading preferences with an old parameter names and types:
+		type PreferencesOld struct {
+			IsLogging                string `json:"enable_logging"`
+			IsFwPersistant           string `json:"firewall_is_persistent"`
+			IsFwAllowLAN             string `json:"firewall_allow_lan"`
+			IsFwAllowLANMulticast    string `json:"firewall_allow_lan_multicast"`
+			IsStopOnClientDisconnect string `json:"is_stop_server_on_client_disconnect"`
+			IsObfsproxy              string `json:"enable_obfsproxy"`
+			OpenVpnExtraParameters   string `json:"open_vpn_extra_parameters"`
+		}
+		oldStylePrefs := &PreferencesOld{}
+
+		if err := json.Unmarshal(data, oldStylePrefs); err != nil {
+			return err
+		}
+
+		s.IsLogging = oldStylePrefs.IsLogging == "1"
+		s.IsFwPersistant = oldStylePrefs.IsFwPersistant == "1"
+		s.IsFwAllowLAN = oldStylePrefs.IsFwAllowLAN == "1"
+		s.IsFwAllowLANMulticast = oldStylePrefs.IsFwAllowLANMulticast == "1"
+		s.IsStopOnClientDisconnect = oldStylePrefs.IsStopOnClientDisconnect == "1"
+		s.IsObfsproxy = oldStylePrefs.IsObfsproxy == "1"
+		s.OpenVpnExtraParameters = oldStylePrefs.OpenVpnExtraParameters
+
+		return nil
 	}
 
 	return json.Unmarshal(data, s)
