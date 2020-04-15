d7ba907985bc4b722adfcd7e5b7c26eed2ad4957
Alexandr Stelnykovych <alexandr.stelnykovych@ivpn.net>
2020-04-15 22:59:57 +0300

    Linux: improvement of 'uninstaller' script
    fix of 'uninstalling' issue on Fedora

--- References/Linux/package_scripts/before-remove.sh
+++ References/Linux/package_scripts/before-remove.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 echo "[+] Disconnecting ..."
-ivpn disconnect || echo "[-] Failed to disconnect"
+/usr/local/bin/ivpn disconnect || echo "[-] Failed to disconnect"
 
 echo "[+] Disabling firewall ..."
-ivpn firewall -off || echo "[-] Failed to disable firewall"
+/usr/local/bin/ivpn firewall -off || echo "[-] Failed to disable firewall"
 
 echo "[+] Logging out ..."
-ivpn logout || echo "[-] Failed to log out"
+/usr/local/bin/ivpn logout || echo "[-] Failed to log out"
 
 echo "[+] Service cleanup (pleaserun) ..."
 sh /usr/share/pleaserun/ivpn-service/generate-cleanup.sh || echo "[-] Service cleanup FAILED!"
