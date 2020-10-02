d5eb4f309d2b43721f064c9df7598aa5d1a88b03
Alexandr Stelnykovych <alexandr.stelnykovych@ivpn.net>
2020-10-02 23:07:11 +0300

    [fix] (Linux) RPM package

--- References/Linux/package_scripts/after-remove.sh
+++ References/Linux/package_scripts/after-remove.sh
@@ -2,18 +2,31 @@
 
 echo "[*] After remove (<%= version %> : <%= pkg %> : $1)"
 
+DESKTOP_FILE=/usr/share/applications/IVPN.desktop
+
 # Obtaining information about user running the script
 # (script can be executed with 'sudo', but we should get real user)
 USER="${SUDO_USER:-$USER}"
 UI_APP_USER_DIR="/home/${SUDO_USER:-$USER}/.config/ivpn-ui"
 
-if [ -d $UI_APP_USER_DIR ] ; then
-  if [ "$1" = "upgrade" ] ; then
+# DEB argument on upgrade - 'upgrade'; RPM - '1'
+if [ "$1" = "upgrade" ] || [ "$1" = "1" ] ; then
+  # UPGRADE
+  if [ -d $UI_APP_USER_DIR ] ; then
     echo "[ ] Upgrade detected"
     echo "    Keeping application cache data from the previous version:"
     echo "    '$UI_APP_USER_DIR'"
-  else
+  fi
+else
+  # REMOVE
+  if [ -f $DESKTOP_FILE ]; then
+    echo "[ ] Uninstalling .desktop file"
+    rm $DESKTOP_FILE || echo "[-] Failed to remove file: '$DESKTOP_FILE'"
+  fi
+
+  if [ -d $UI_APP_USER_DIR ] ; then
     echo "[ ] Removing application cache data from the previous version"
     rm -rf $UI_APP_USER_DIR || echo "[-] Failed to erase cache data in: '$UI_APP_USER_DIR'"
   fi
 fi
+
--- References/Linux/package_scripts/before-remove.sh
+++ References/Linux/package_scripts/before-remove.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-echo "[*] Before remove (<%= version %> : <%= pkg %> : $1)"
-
-DESKTOP_FILE=/usr/share/applications/IVPN.desktop
-if [ -f $DESKTOP_FILE ]; then
-    echo "[ ] Uninstalling .desktop file"
-    rm $DESKTOP_FILE || echo "[-] Failed to remove file: '$DESKTOP_FILE'"
-fi
+#echo "[*] Before remove (<%= version %> : <%= pkg %> : $1)"
