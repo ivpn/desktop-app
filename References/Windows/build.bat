68eb9e2a1fc50af4aaf460296118fe3eb9f628ba
Alexandr Stelnykovych <alexandr.stelnykovych@ivpn.net>
2021-01-08 16:41:59 +0200

    Introduce Windows Installer
    https://privatus.atlassian.net/browse/WC-1039

---
+++ References/Windows/build.bat
@@ -0,0 +1,159 @@
+@echo off
+
+setlocal
+set SCRIPTDIR=%~dp0
+
+rem ==================================================
+rem DEFINE path to NSIS binary here
+SET MAKENSIS="C:\Program Files (x86)\NSIS\makensis.exe"
+rem ==================================================
+SET INSTALLER_OUT_DIR=%SCRIPTDIR%bin
+set INSTALLER_TMP_DIR=%INSTALLER_OUT_DIR%\temp
+SET FILE_LIST=%SCRIPTDIR%Installer\release-files.txt
+
+set APPVER=???
+set SERVICE_REPO=%SCRIPTDIR%..\..\..\desktop-app-daemon
+set CLI_REPO=%SCRIPTDIR%..\..\..\desktop-app-cli
+
+rem Checking if msbuild available
+WHERE msbuild >nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+	echo [!] 'msbuild' is not recognized as an internal or external command
+	echo [!] Ensure you are running this script from Developer Cammand Prompt for Visual Studio
+	goto :error
+)
+
+rem Checking if NSIS  available
+if not exist %MAKENSIS% (
+    echo [!] NSIS binary not found [%MAKENSIS%]
+	echo [!] Install NSIS [https://nsis.sourceforge.io/] or\and modify MAKENSIS variable of this script
+	goto :error
+)
+
+call :read_app_version 				|| goto :error
+echo     APPVER         : '%APPVER%'
+echo     SOURCES Service: %SERVICE_REPO%
+echo     SOURCES CLI    : %CLI_REPO%
+
+rem call :build_service						|| goto :error
+rem call :build_cli								|| goto :error
+rem call :build_ui								|| goto :error
+rem
+call :copy_files 							|| goto :error
+call :build_installer					|| goto :error
+
+rem THE END
+goto :success
+
+:read_app_version
+	echo [*] Reading App version ...
+
+	set VERSTR=???
+	set PackageJsonFile=%SCRIPTDIR%..\..\package.json
+	set VerRegExp=^ *\"version\": *\".*\", *$
+
+	set cmd=findstr /R /C:"%VerRegExp%" "%PackageJsonFile%"
+	rem Find string in file
+	FOR /F "tokens=* USEBACKQ" %%F IN (`%cmd%`) DO SET VERSTR=%%F
+	if	"%VERSTR%" == "???" (
+		echo "[!] ERROR: The file '%AssemblyInfFile%' shall contain '[assembly: AssemblyVersion("X.X.X")]' string"
+		exit /b 1
+ 	)
+	rem Get substring in quotes
+	for /f tokens^=3^ delims^=^" %%a in ("%VERSTR%") do (
+			set APPVER=%%a
+	)
+
+	goto :eof
+
+:build_service
+	echo [*] Building IVPN service and dependencies...
+	call %SERVICE_REPO%\References\Windows\scripts\build-all.bat %APPVER% || exit /b 1
+	goto :eof
+
+:build_cli
+	echo [*] Building IVPN CLI...
+	echo %CLI_REPO%\References\Windows\build.bat
+	call %CLI_REPO%\References\Windows\build.bat %APPVER% || exit /b 1
+	goto :eof
+
+:build_ui
+	echo [*] Building UI...
+  cd %SCRIPTDIR%\..\..  || exit /b 1
+	call npm run electron:build  || exit /b 1
+	cd %SCRIPTDIR%  || exit /b 1
+	goto :eof
+
+:copy_files
+	echo [*] Copying files...
+
+	IF exist "%INSTALLER_TMP_DIR%" (
+		rmdir /s /q "%INSTALLER_TMP_DIR%"
+	)
+	mkdir "%INSTALLER_TMP_DIR%"
+
+	set UI_BINARIES_FOLDER=%SCRIPTDIR%..\..\dist_electron\win-unpacked
+	echo     Copying UI '%UI_BINARIES_FOLDER%' ...
+	xcopy /E /I  "%UI_BINARIES_FOLDER%" "%INSTALLER_TMP_DIR%\ui" || goto :error
+	echo     Renaming UI binary to 'IVPN Client.exe' ...
+	rename  "%INSTALLER_TMP_DIR%\ui\ivpn-ui.exe" "IVPN Client.exe" || goto :error
+
+	echo     Copying other files ...
+	set BIN_FOLDER_SERVICE=%SERVICE_REPO%\bin\x86_64\
+	set BIN_FOLDER_SERVICE_REFS=%SERVICE_REPO%\References\Windows\
+	set BIN_FOLDER_CLI=%CLI_REPO%\bin\x86_64\
+
+	setlocal EnableDelayedExpansion
+	for /f "tokens=*" %%i in (%FILE_LIST%) DO (
+		set SRCPATH=???
+		if exist "%BIN_FOLDER_SERVICE%%%i" set SRCPATH=%BIN_FOLDER_SERVICE%%%i
+		if exist "%BIN_FOLDER_CLI%%%i" set SRCPATH=%BIN_FOLDER_CLI%%%i
+		if exist "%BIN_FOLDER_SERVICE_REFS%%%i" set SRCPATH=%BIN_FOLDER_SERVICE_REFS%%%i
+		if exist "%BIN_FOLDER_APP%%%i"  set SRCPATH=%BIN_FOLDER_APP%%%i
+		if exist "%SCRIPTDIR%Installer\%%i" set SRCPATH=%SCRIPTDIR%Installer\%%i
+		if !SRCPATH! == ??? (
+			echo FILE '%%i' NOT FOUND!
+			exit /b 1
+		)
+		echo     !SRCPATH!
+
+		IF NOT EXIST "%INSTALLER_TMP_DIR%\%%i\.." (
+			MKDIR "%INSTALLER_TMP_DIR%\%%i\.."
+		)
+
+		copy /y "!SRCPATH!" "%INSTALLER_TMP_DIR%\%%i" > NUL
+		IF !errorlevel! NEQ 0 (
+			ECHO     Error: failed to copy "!SRCPATH!" to "%INSTALLER_TMP_DIR%"
+			EXIT /B 1
+		)
+	)
+	goto :eof
+
+:build_installer
+	echo [*] Building installer...
+	cd %SCRIPTDIR%\Installer
+
+	SET OUT_FILE="%INSTALLER_OUT_DIR%\IVPN-Client-v%APPVER%.exe"
+	%MAKENSIS% /DPRODUCT_VERSION=%APPVER% /DOUT_FILE=%OUT_FILE% /DSOURCE_DIR=%INSTALLER_TMP_DIR% "IVPN Client.nsi"
+	IF not ERRORLEVEL 0 (
+		ECHO [!] Error: failed to create installer
+		EXIT /B 1
+	)
+	goto :eof
+
+:success
+	goto :remove_tmp_vars_before_exit
+	echo [*] SUCCESS
+	exit /b 0
+
+:error
+	goto :remove_tmp_vars_before_exit
+	echo [!] IVPN Client installer build FAILED with error #%errorlevel%.
+	exit /b %errorlevel%
+
+:remove_tmp_vars_before_exit
+	endlocal
+	rem Removing temporary global variables
+	set IVPN_GOROOT=
+	set IVPN_PATH=
+	goto :eof
