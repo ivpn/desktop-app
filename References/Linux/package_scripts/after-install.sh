022cc0fe62dfdaecae5cb0f3e21f8a14c6c6fad7
Alexandr Stelnykovych <alexandr.stelnykovych@ivpn.net>
2021-02-19 19:02:34 +0200

    [fix] (Linux) Hardcoded paths lead to errors in certain distributions

--- References/Linux/package_scripts/after-install.sh
+++ References/Linux/package_scripts/after-install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-/usr/bin/echo "[*] After install (<%= pkg %>)"
+echo "[*] After install (<%= pkg %>)"
 
 NEED_TO_SAVE_INSTRUCTIONS=true
 IVPN_ETC="/opt/ivpn/etc"
 IVPN_TMP="/opt/ivpn/mutable"
 INSTRUCTIONS_FILE="$IVPN_TMP/service_install.txt"
-/usr/bin/mkdir -p $IVPN_TMP
-[ -e $INSTRUCTIONS_FILE ] && /usr/bin/rm $INSTRUCTIONS_FILE
+mkdir -p $IVPN_TMP
+[ -e $INSTRUCTIONS_FILE ] && rm $INSTRUCTIONS_FILE
 
 
 silent() {
@@ -16,58 +16,58 @@ silent() {
 
 has_systemd() {
   # Some OS vendors put systemd in ... different places ...
-  [ -d "/lib/systemd/system/" -o -d "/usr/lib/systemd/system" ] && silent /usr/bin/which systemctl
+  [ -d "/lib/systemd/system/" -o -d "/usr/lib/systemd/system" ] && silent which systemctl
 }
 
 try_systemd_install() {
     if has_systemd ; then
-        /usr/bin/echo "[ ] systemd detected. Trying to start service ..."
-        /usr/bin/echo "[+] Stopping old service (if exists)"
-        /usr/bin/systemctl stop ivpn-service
-        /usr/bin/echo "[+] Enabling service"
-        /usr/bin/systemctl enable ivpn-service || return 1
-        /usr/bin/echo "[+] Starting service"
-        /usr/bin/systemctl start ivpn-service || return 1
+        echo "[ ] systemd detected. Trying to start service ..."
+        echo "[+] Stopping old service (if exists)"
+        systemctl stop ivpn-service
+        echo "[+] Enabling service"
+        systemctl enable ivpn-service || return 1
+        echo "[+] Starting service"
+        systemctl start ivpn-service || return 1
 
         NEED_TO_SAVE_INSTRUCTIONS=false
         return 0
     else
-        /usr/bin/echo "[-] Unable to start service automatically"
+        echo "[-] Unable to start service automatically"
     fi
 }
 
-/usr/bin/echo "[+] Defining access rights for files ..."
-silent /usr/bin/chmod 0400 $IVPN_ETC/*             # can read only owner (root)
-silent /usr/bin/chmod 0600 $IVPN_ETC/servers.json  # can read/wrire only owner (root)
-silent /usr/bin/chmod 0700 $IVPN_ETC/*.sh          # can execute only owner (root)
-silent /usr/bin/chmod 0700 $IVPN_ETC/*.up          # can execute only owner (root)
-silent /usr/bin/chmod 0700 $IVPN_ETC/*.down        # can execute only owner (root)
-silent /usr/bin/chmod 0755 /usr/local/bin/ivpn         # can change only owner (root)
-silent /usr/bin/chmod 0755 /usr/local/bin/ivpn-service # can change only owner (root)
+echo "[+] Defining access rights for files ..."
+silent chmod 0400 $IVPN_ETC/*             # can read only owner (root)
+silent chmod 0600 $IVPN_ETC/servers.json  # can read/wrire only owner (root)
+silent chmod 0700 $IVPN_ETC/*.sh          # can execute only owner (root)
+silent chmod 0700 $IVPN_ETC/*.up          # can execute only owner (root)
+silent chmod 0700 $IVPN_ETC/*.down        # can execute only owner (root)
+silent chmod 0755 /usr/local/bin/ivpn         # can change only owner (root)
+silent chmod 0755 /usr/local/bin/ivpn-service # can change only owner (root)
 
 
 IVPN_SAVED_DNS_FILE="/etc/resolv.conf.ivpnsave"
 if [ -f $IVPN_SAVED_DNS_FILE ]; then
-  /usr/bin/echo "[+] restoring DNS configuration from previous installation"
-  /usr/bin/mv $IVPN_SAVED_DNS_FILE /etc/resolv.conf || /usr/bin/echo "[-] Restoring DNS failed"
+  echo "[+] restoring DNS configuration from previous installation"
+  mv $IVPN_SAVED_DNS_FILE /etc/resolv.conf || echo "[-] Restoring DNS failed"
 fi
 
-/usr/bin/echo "[+] Service install start (pleaserun) ..."
-INSTALL_OUTPUT=$(/usr/bin/sh /usr/share/pleaserun/ivpn-service/install.sh)
+echo "[+] Service install start (pleaserun) ..."
+INSTALL_OUTPUT=$(sh /usr/share/pleaserun/ivpn-service/install.sh)
 if [ $? -eq 0 ]; then
     # Print output of the install script
-    /usr/bin/echo $INSTALL_OUTPUT
+    echo $INSTALL_OUTPUT
 
     try_systemd_install
 else
     # Print output of the install script
-    /usr/bin/echo $INSTALL_OUTPUT
-    /usr/bin/echo "[-] Service install FAILED!"
+    echo $INSTALL_OUTPUT
+    echo "[-] Service install FAILED!"
 fi
 
 if $NEED_TO_SAVE_INSTRUCTIONS == true ; then
-    /usr/bin/echo $INSTALL_OUTPUT > $INSTRUCTIONS_FILE
-    /usr/bin/echo "[!] Service start instructions saved into file: '$INSTRUCTIONS_FILE'"
+    echo $INSTALL_OUTPUT > $INSTRUCTIONS_FILE
+    echo "[!] Service start instructions saved into file: '$INSTRUCTIONS_FILE'"
 fi
 
 FILE_ACCID_TO_UPGRADE="/opt/ivpn/mutable/toUpgradeID.tmp"
@@ -75,21 +75,21 @@ if [ -f $FILE_ACCID_TO_UPGRADE ]; then
   # It is an upgrade.
   # We need to re-login after installation finished.
   # Read account ID
-  ACCID=$(/usr/bin/cat $FILE_ACCID_TO_UPGRADE) || /usr/bin/echo "[-] Finishing installation: Failed to read accountID to re-login"
+  ACCID=$(cat $FILE_ACCID_TO_UPGRADE) || echo "[-] Finishing installation: Failed to read accountID to re-login"
 
   # do not forget to remove temporary file
-  silent /usr/bin/rm $FILE_ACCID_TO_UPGRADE
+  silent rm $FILE_ACCID_TO_UPGRADE
 
   if [ ! -z "$ACCID" ]; then
     # giving a chance for a daemon to fully start
-    /usr/bin/sleep 1
-    /usr/bin/echo "[+] Logging in ..."
+    sleep 1
+    echo "[+] Logging in ..."
     /usr/local/bin/ivpn login $ACCID #||  echo "[-] Finishing installation: Failed to to re-login (try#1)"
     if [ ! $? -eq 0 ]; then
-      /usr/bin/echo "[-] Finishing installation: Failed to to re-login (try#1)"
-      /usr/bin/echo "[ ] Retry ..."
-      /usr/bin/sleep 3
-      /usr/local/bin/ivpn login $ACCID ||  /usr/bin/echo "[-] Finishing installation: Failed to to re-login (try#2)"
+      echo "[-] Finishing installation: Failed to to re-login (try#1)"
+      echo "[ ] Retry ..."
+      sleep 3
+      /usr/local/bin/ivpn login $ACCID ||  echo "[-] Finishing installation: Failed to to re-login (try#2)"
     fi
   fi
 fi
--- References/Linux/package_scripts/before-remove.sh
+++ References/Linux/package_scripts/before-remove.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-/usr/bin/echo "[*] Before remove (<%= pkg %>)"
+echo "[*] Before remove (<%= pkg %>)"
 
 PKG_TYPE=<%= pkg %>
 
-/usr/bin/echo "[+] Disabling firewall (before remove) ..."
-/usr/local/bin/ivpn firewall -off || /usr/bin/echo "[-] Failed to disable firewall"
+echo "[+] Disabling firewall (before remove) ..."
+/usr/local/bin/ivpn firewall -off || echo "[-] Failed to disable firewall"
 
-/usr/bin/echo "[+] Disconnecting (before remove) ..."
-/usr/local/bin/ivpn disconnect || /usr/bin/echo "[-] Failed to disconnect"
+echo "[+] Disconnecting (before remove) ..."
+/usr/local/bin/ivpn disconnect || echo "[-] Failed to disconnect"
 
 if [ "$PKG_TYPE" = "rpm" ]; then
     if [ -f /opt/ivpn/mutable/rpm_upgrade.lock ]; then
-        /usr/bin/echo "[ ] Upgrade detected. Remove operations skipped"
+        echo "[ ] Upgrade detected. Remove operations skipped"
         exit 0
     fi
 fi
@@ -20,15 +20,15 @@ fi
 if [ -f /opt/ivpn/mutable/settings.json ]; then
     # In case of installing new version, we have to login back with current logged-in accountID after installation finished.
     # Therefore we are saving accountID into temporary file (will be deleted after 'after_install' script execution)
-    /usr/bin/echo "[+] Preparing upgrade data ..."
-    ACCID=$(/usr/bin/cat /opt/ivpn/mutable/settings.json | /usr/bin/grep -o \"AccountID\":\"[a-zA-Z0-9]*\" | /usr/bin/cut -d '"' -f 4) || /usr/bin/echo "[-] Failed to read accountID"
+    echo "[+] Preparing upgrade data ..."
+    ACCID=$(cat /opt/ivpn/mutable/settings.json | grep -o \"AccountID\":\"[a-zA-Z0-9]*\" | cut -d '"' -f 4) || echo "[-] Failed to read accountID"
     if [ ! -z "$ACCID" ]; then
-        /usr/bin/echo $ACCID > /opt/ivpn/mutable/upgradeID.tmp || /usr/bin/echo "[-] Failed to save accountID into temporary file"
+        echo $ACCID > /opt/ivpn/mutable/upgradeID.tmp || echo "[-] Failed to save accountID into temporary file"
     fi
 fi
 
-/usr/bin/echo "[+] Logging out ..."
-/usr/local/bin/ivpn logout || /usr/bin/echo "[-] Failed to log out"
+echo "[+] Logging out ..."
+/usr/local/bin/ivpn logout || echo "[-] Failed to log out"
 
-/usr/bin/echo "[+] Service cleanup (pleaserun) ..."
-/usr/bin/sh /usr/share/pleaserun/ivpn-service/generate-cleanup.sh || /usr/bin/echo "[-] Service cleanup FAILED!"
+echo "[+] Service cleanup (pleaserun) ..."
+sh /usr/share/pleaserun/ivpn-service/generate-cleanup.sh || echo "[-] Service cleanup FAILED!"
