28b38ccb44a3ecf8c8915e4789b3e875b3ecdf00
Alexandr Stelnykovych <alexandr.stelnykovych@ivpn.net>
2021-02-26 11:31:19 +0200

    (Windows) Build fix

--- wifiNotifier/wifiNotifier_windows_amd64.go
+++ wifiNotifier/wifiNotifier_windows_amd64.go
@@ -343,21 +343,21 @@ func GetCurrentSSID() string {
 // GetCurrentNetworkIsInsecure returns current security mode
 func GetCurrentNetworkIsInsecure() bool {
 	const (
-		DOT11_CIPHER_ALGO_NONE           = 0x00
-		DOT11_CIPHER_ALGO_WEP40          = 0x01 
-		DOT11_CIPHER_ALGO_TKIP           = 0x02
-		DOT11_CIPHER_ALGO_CCMP           = 0x04
-		DOT11_CIPHER_ALGO_WEP104         = 0x05 
-		DOT11_CIPHER_ALGO_WPA_USE_GROUP  = 0x100
-		DOT11_CIPHER_ALGO_RSN_USE_GROUP  = 0x100
-		DOT11_CIPHER_ALGO_WEP            = 0x101
-		DOT11_CIPHER_ALGO_IHV_START      = 0x80000000
-		DOT11_CIPHER_ALGO_IHV_END        = 0xffffffff
+		DOT11_CIPHER_ALGO_NONE          = 0x00
+		DOT11_CIPHER_ALGO_WEP40         = 0x01
+		DOT11_CIPHER_ALGO_TKIP          = 0x02
+		DOT11_CIPHER_ALGO_CCMP          = 0x04
+		DOT11_CIPHER_ALGO_WEP104        = 0x05
+		DOT11_CIPHER_ALGO_WPA_USE_GROUP = 0x100
+		DOT11_CIPHER_ALGO_RSN_USE_GROUP = 0x100
+		DOT11_CIPHER_ALGO_WEP           = 0x101
+		DOT11_CIPHER_ALGO_IHV_START     = 0x80000000
+		DOT11_CIPHER_ALGO_IHV_END       = 0xffffffff
 	)
 
 	security := C.getCurrentNetworkSecurity()
 	switch security {
-	case: DOT11_CIPHER_ALGO_NONE,
+	case DOT11_CIPHER_ALGO_NONE,
 		DOT11_CIPHER_ALGO_WEP40,
 		DOT11_CIPHER_ALGO_WEP104,
 		DOT11_CIPHER_ALGO_WEP:
