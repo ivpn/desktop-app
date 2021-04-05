6604db6b8ccab298e0befab4d4b8315829dfd47d
Alexandr Stelnykovych <alexandr.stelnykovych@ivpn.net>
2021-04-05 09:01:17 +0300

    Captcha
    https://privatus.atlassian.net/browse/WC-1095

--- src/api/statuscode.js
+++ src/api/statuscode.js
@@ -22,3 +22,9 @@
 
 export const API_SUCCESS = 200;
 export const API_SESSION_LIMIT = 602;
+
+export const API_CAPTCHA_REQUIRED = 70001;
+export const API_CAPTCHA_INVALID = 70002;
+
+export const API_2FA_REQUIRED = 70011; // Account has two-factor authentication enabled. Please enter TOTP token to login
+export const API_2FA_TOKEN_NOT_VALID = 70012; // Specified two-factor authentication token is not valid
