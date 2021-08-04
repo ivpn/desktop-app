087e0001dd04283511c56b02da688e9dc1bc56c1
Alexandr Stelnykovych <alexandr.stelnykovych@ivpn.net>
2021-08-04 13:57:25 +0300

    (Linux) RPM: excluded '/usr/lib/.build-id' folder
    https://github.com/ivpn/desktop-app/issues/22

--- cli/References/Linux/build.sh
+++ cli/References/Linux/build.sh
@@ -140,6 +140,7 @@ CreatePackage()
   # NOTE! 'remove' scripts is using from old version!
 
   fpm -d openvpn $EXTRA_ARGS \
+    --rpm-rpmbuild-define "_build_id_links none" \
     --deb-no-default-config-files -s dir -t $PKG_TYPE -n ivpn -v $VERSION --url https://www.ivpn.net --license "GNU GPL3" \
     --template-scripts --template-value pkg=$PKG_TYPE \
     --vendor "Privatus Limited" --maintainer "Privatus Limited" \
