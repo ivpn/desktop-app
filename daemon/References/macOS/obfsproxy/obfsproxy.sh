4c9702ea323d21db4dbeefbd9a583d97070ee27c
Alexandr Stelnykovych <alexandr.stelnykovych@ivpn.net>
2021-09-22 11:04:50 +0200

    (macOS) Running obfsproxy binaries on arm64 (using Rosetta) https://github.com/ivpn/desktop-app-shadow/issues/15

--- daemon/References/macOS/obfsproxy/obfsproxy.sh
+++ daemon/References/macOS/obfsproxy/obfsproxy.sh
@@ -3,4 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${DIR}/obfsproxy"
 
-exec /System/Library/Frameworks/Python.framework/Versions/2.6/bin/python ./obfsproxy.bin "$@"
+exec arch -x86_64 /System/Library/Frameworks/Python.framework/Versions/2.6/bin/python ./obfsproxy.bin "$@"
