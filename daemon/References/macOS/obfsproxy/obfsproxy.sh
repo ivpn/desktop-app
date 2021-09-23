d4ec195ed8818e7f8d92cc2f3c89eed9f2e30681
Alexandr Stelnykovych <alexandr.stelnykovych@ivpn.net>
2021-09-23 12:42:08 +0300

    Removed redundant script

--- daemon/References/macOS/obfsproxy/obfsproxy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "${DIR}/obfsproxy"
-
-exec arch -x86_64 /System/Library/Frameworks/Python.framework/Versions/2.6/bin/python ./obfsproxy.bin "$@"
