0bbbbbdebaf26e70d3dfe87f38edac87ef23ddd8
Alexandr Stelnykovych <alexandr.stelnykovych@ivpn.net>
2022-12-12 13:57:13 +0200

    UI: allow change settings in a connected state (and reconnect then if required)
    https://github.com/ivpn/desktop-app/issues/125
    https://github.com/ivpn/desktop-app/issues/184

--- ui/src/components/blocks/block-hop-buttons.vue
+++ ui/src/components/blocks/block-hop-buttons.vue
@@ -32,32 +32,36 @@
 <script>
 const sender = window.ipcSender;
 
-import { VpnStateEnum } from "@/store/types";
-
 export default {
   computed: {},
 
   methods: {
-    ChangeHop(isMultihop) {
+    async ChangeHop(isMultihop) {
       if (this.$store.state.settings.isMultiHop === isMultihop) return;
-      if (
-        this.$store.state.vpnState.connectionState !== VpnStateEnum.DISCONNECTED
-      ) {
-        sender.showMessageBox({
-          type: "info",
-          buttons: ["OK"],
-          message: "You are now connected to IVPN",
-          detail:
-            "You can change Multi-Hop settings only when IVPN is disconnected.",
-        });
-        return;
-      }
 
       this.$store.dispatch(
         `settings/isMultiHop`,
         !this.$store.state.settings.isMultiHop
       );
+
+      if (
+        this.$store.getters["vpnState/isConnected"] ||
+        this.$store.getters["vpnState/isConnecting"]
+      ) {
+        // Re-connect
+        try {
+          await sender.Connect();
+        } catch (e) {
+          console.error(e);
+          sender.showMessageBoxSync({
+            type: "error",
+            buttons: ["OK"],
+            message: `Failed to connect: ` + e,
+          });
+        }
+      }
     },
+
     showServersList(isExitServer) {
       this.onShowServersPressed(isExitServer);
     },
