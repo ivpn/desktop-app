6bed0c47b2589f785023c07e4135bbb3943f0b5c
Alexandr Stelnykovych <alexandr.stelnykovych@ivpn.net>
2023-07-18 16:45:07 +0300

    (Linux) DEB/RPM package: added dependency from 'iptables' package
    https://github.com/ivpn/desktop-app/issues/286

--- cli/References/Linux/build.sh
+++ cli/References/Linux/build.sh
@@ -200,7 +200,7 @@ CreatePackage()
   #   [*] Before remove (3.3.30 : rpm : 0)
   #   [*] After remove (3.3.30 : rpm : 0)
 
-  fpm -d openvpn $EXTRA_ARGS \
+  fpm -d openvpn -d iptables $EXTRA_ARGS \
     --rpm-rpmbuild-define "_build_id_links none" \
     --deb-no-default-config-files -s dir -t $PKG_TYPE -n ivpn -v $VERSION --url https://www.ivpn.net --license "GNU GPL3" \
     --template-scripts --template-value pkg=$PKG_TYPE \
