1df08c26980d8e998e7e4ea423c14dce3ed28ce0
Alexandr Stelnykovych <alexandr.stelnykovych@ivpn.net>
2023-09-01 14:49:13 +0300

    Modified the build script for V2Ray to enable compilation from AUR using the latest Go version. This is a temporary solution until the next official V2Ray version released (greater than v5.7.0).

--- daemon/References/Linux/scripts/build-v2ray.sh
+++ daemon/References/Linux/scripts/build-v2ray.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-V2RAY_VER=v5.7.0
+# NOTE!!! Special build script for AUR repository:
+# Version v5.7.0 (which is the latest at the current moment) of V2Ray cannot be compiled with Go 1.21 yet.
+# Therefore, we are using the latest V2Ray sources available at the time of writing this script. 
+# These sources have not been updated to a new version but can be compiled with Go 1.21.
+
+# V2RAY_VER=v5.7.0
 
 # Exit immediately if a command exits with a non-zero status.
 set -e
@@ -19,8 +24,12 @@ mkdir -pv ${INSTALL_DIR}
 
 echo "******** Cloning V2Ray sources..."
 cd ${BUILD_DIR}
-git clone  --depth 1 --branch ${V2RAY_VER} https://github.com/v2fly/v2ray-core.git
-cd v2ray-core/main
+#git clone  --depth 1 --branch ${V2RAY_VER} https://github.com/v2fly/v2ray-core.git
+# cd v2ray-core/main
+git clone --shallow-since="2023-08-30" https://github.com/v2fly/v2ray-core.git
+cd v2ray-core
+git checkout 87155bfc7193efd348db05df7c31e6ae71d890fe # Commit: Aug 31, 2023 (latest commit on a moment writing this line)
+cd main
 
 echo "******** Compiling 'V2Ray'..."
 go build -o ${INSTALL_DIR}/v2ray -trimpath -ldflags "-s -w"
