13e24449ec4b1178a29acfa2f2d093a8d0c08960
Rolando Townsend <lecnfes9kr@victoriaalison.com>
2024-09-20 14:06:54 +0000

    Update Component-Settings.vue

--- ui/src/views/Component-Settings.vue
+++ ui/src/views/Component-Settings.vue
@@ -74,7 +74,7 @@
                   activeBtn: view === 'networks',
                 }"
               >
-                WiFi control
+                Wi-Fi control
               </button>
 
               <button
